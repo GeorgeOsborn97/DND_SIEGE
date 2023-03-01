a53589888da866d363cf07aee2969a4f6135fe4e
GeorgeOsborn97 <George.b.osborn@outlook.com>
2023-03-01 13:19:39 +0000

    install summernote

--- siege_manager/admin.py
+++ siege_manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Role, Operation, People, Supplies
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Role, Operation, People, Supplies)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description', 'express_cost')
--- siege_manager/models.py
+++ siege_manager/models.py
@@ -5,25 +5,37 @@ from django.db import models
 
 class Supplies(models.Model):
     supply = models.CharField(max_length=200, unique=True, primary_key=True)
-    description = models.TextField(blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.supply
 
 
 class People(models.Model):
     occupation = models.CharField(max_length=200, unique=True, primary_key=True)
-    description = models.TextField(blank=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.occupation
 
 
 class Operation(models.Model):
     title = models.CharField(max_length=50, primary_key=True)
     slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField(blank=True)
+    description = models.TextField()
     resources_required = models.ManyToManyField(Supplies, related_name='resources_req')
     people_required = models.ManyToManyField(People, related_name='people_req')
-    express_cost = models.TextField(blank=True)
+    express_cost = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 
 class Role(models.Model):
     title = models.CharField(max_length=50, primary_key=True)
     slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField(blank=True)
+    description = models.TextField()
     operation_options = models.ManyToManyField(Operation, related_name='ops')
+
+    def __str__(self):
+        return self.title
